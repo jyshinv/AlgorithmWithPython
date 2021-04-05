522b5c971f99a515c6877e333e9bc014bfd3d198
jyshinv <sjy1218vvv@gmail.com>
2021-04-05 19:29:44 +0900

    최단경로_개선된 다익스트라 알고리즘 인접리스트, 인접행렬 이용해서 코딩완료

--- 이것이코테_최단경로_개선된 다익스트라 알고리즘 인접행렬로 바꿔보기.py
+++ 이것이코테_최단경로_개선된 다익스트라 알고리즘 인접행렬로 바꿔보기.py
@@ -8,9 +8,11 @@ n, m = map(int, input().split())
 #시작 노드 번호를 입력받기
 start = int(input())
 #각 노드에 연결되어 있는 노드에 대한 정보를 담는 리스트를 만들기
-graph=[[0]*(n+1) for _ in range(n+1)]
+graph=[[INF]*(n+1) for _ in range(n+1)]
 #최단 거리 테이블을 모두 무한으로 초기화
 distance = [INF]*(n+1)
+#경로 출력을 위한 list n+1개의 행 만들기
+arr=[[] for _ in range(n+1)]
 
 #모든 간선 정보를 입력받기
 for _ in range(m) :
@@ -23,7 +25,6 @@ def dijkstra(start) :
     q=[]
     #시작 노드로 가기 위한 최단 경로는 0으로 설정하여, 큐에 삽입
     heapq.heappush(q,(0, start))
-    distance[start] = 0
     while q: #큐가 비어있지 않다면
         #가장 최단 거리가 짧은 노드에 대한 정보 꺼내기
         dist, now = heapq.heappop(q)
@@ -33,7 +34,7 @@ def dijkstra(start) :
         #현재 노드와 연결된 다른 인접한 노드들을 확인
         #노드 0부터 n까지 확인 (i=0~n)
         for i in range(n+1) :
-            if graph[now][i] != 0 :
+            #if graph[now][i] != 0 :
                 cost = dist + graph[now][i]
                 #현재 노드를 거쳐서, 다른 노드를 이동하는 거리가 더 짧은 경우
                 if cost < distance[i]:
